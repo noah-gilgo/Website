9d1936e75666a8e918c474eb527a20ae001cb152
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2021-07-13 15:20:23 -0400

    Created functions.js and an index.html page for the blog directory.

---
+++ functions.js
@@ -0,0 +1,16 @@
+/*function isImposter(amogus) {
+    if(!amogus) {
+        alert("It's just a sussy baka and it cannot be that bad");
+    } else {
+        alert("I'm feeling like imposter; I might just be a monster");
+    }
+}*/
+
+function ternaryOperator(condition, op1, op2) {
+    //why did I make this
+    if(condition) {
+        return op1;
+    } else {
+        return op2;
+    }
+}
