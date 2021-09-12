e241d7deab7305923252da4a7cd9fbae3f3d7b3e
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2021-09-12 17:57:18 -0400

    Made some changes to HTML and JS in the homepage.

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function howOldAmI() {
     var myBirthday = 905382600000; // My birthday measured in milliseconds elapsed since the start of January 1st, 1970.
-    var now = Date.now;
-    return Math.abs((now - myBirthday) / 31556952000).toString(10);
+    var now = Date.now();
+    return Math.floor((now - myBirthday) / 31556952000);
 }
