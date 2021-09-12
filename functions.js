c0409cc9b58f9a7a611b44f243608554690c00f2
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2021-09-12 17:21:14 -0400

    Tried to integrate a JavaScript function into my homepage

--- functions.js
+++ functions.js
@@ -1,22 +1,5 @@
-/*function isImposter(amogus) {
-    if(!amogus) {
-        alert("It's just a sussy baka and it cannot be that bad");
-    } else {
-        alert("I'm feeling like imposter; I might just be a monster");
-    }
-}*/
-
-function ternaryOperator(condition, op1, op2) {
-    //why did I make this
-    if(condition) {
-        return op1;
-    } else {
-        return op2;
-    }
-}
-
 function howOldAmI() {
-    myBirthday = 905382600000 // My birthday measured in milliseconds elapsed since the start of January 1st, 1970.
-    howOldIAm = new Date(Date.now - myBirthday);
-    return (howOldIAm.getYear() - 1970);
+    var myBirthday = 905382600000; // My birthday measured in milliseconds elapsed since the start of January 1st, 1970.
+    var now = Date.now;
+    return Math.abs((now - myBirthday) / 31556952000).toString(10);
 }
