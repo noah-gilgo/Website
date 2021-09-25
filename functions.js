5ce054a14b47bdf4d96dd9ed6bd3a17908522e28
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2021-09-25 13:41:57 -0400

    Added howOldAmI function to the homepage html file

--- functions.js
+++ functions.js
@@ -3,3 +3,12 @@ function howOldAmI() {
     var now = Date.now();
     return Math.floor((now - myBirthday) / 31556952000);
 }
+
+function addText() {
+    // Functions like this make me really want to learn Angular
+    if(!document.getElementById) return;
+    var sentence = document.changeform.sentence.value;
+    var node = document.createTextNode(" " + sentence);
+    document.getElementById("paragraph1").appendChild(node);
+    document.changeform.sentence.value="";
+}
